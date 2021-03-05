5e5ccfd9e0e4ca742fe38730c64cd01465b46d9c
Edmunds Folkmanis <folkmanis@gmail.com>
2021-03-05 12:06:12 +0000

    corrected types

--- projects/prd-cdk/src/lib/reactive/log.ts
+++ projects/prd-cdk/src/lib/reactive/log.ts
@@ -1,8 +1,8 @@
 import { pipe, MonoTypeOperatorFunction } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
-export function log<T>(messageOrPredicate: string | ((val: any) => any), message?: string): MonoTypeOperatorFunction<T> {
-    const predicate = typeof messageOrPredicate === 'function' ? messageOrPredicate : (obj: any) => obj;
+export function log<T>(messageOrPredicate: string | ((val: T) => any), message?: string): MonoTypeOperatorFunction<T> {
+    const predicate = typeof messageOrPredicate === 'function' ? messageOrPredicate : (obj: T) => obj;
     message = typeof messageOrPredicate === 'string' ? messageOrPredicate : message;
     if (message) {
         return pipe(
