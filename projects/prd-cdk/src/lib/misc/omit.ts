ff56a8d14a819b67bd9078d9ae8a5d95f2ef636e
Edmunds Folkmanis <folkmanis@gmail.com>
2021-05-14 16:49:21 +0000

    only string properties

--- projects/prd-cdk/src/lib/misc/omit.ts
+++ projects/prd-cdk/src/lib/misc/omit.ts
@@ -4,7 +4,7 @@
  * @param keys object keys to exclude
  * @returns new object without selected keys
  */
-export function omit<T, K extends keyof T>(obj: T, ...keys: K[]): Omit<T, K> {
+export function omit<T, K extends keyof T & string>(obj: T, ...keys: K[]): Omit<T, K> {
     return Object.assign(
         {},
         ...(Object.keys(obj) as Array<keyof T>).filter(key => keys.every(k => k !== key)).map(key => ({ [key]: obj[key] }))
